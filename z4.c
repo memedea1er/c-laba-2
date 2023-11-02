a2463aeba4e08f20fec939e80b04705273119de7
Глеб Кот <116066796+memedea1er@users.noreply.github.com>
2023-11-02 12:43:37 +0500

    Update z4.c

--- z4.c
+++ z4.c
@@ -1,80 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct stack // структура данных для представления stack
+struct Item // структура для представления элемента стека
 {
-    int maxsize; // максимальная емкость stack
-    int top;     // индекс вершины stack
-    int *items;  // массив элементов stack
+    int data;        // значение элемента
+    struct Item *prev; // указатель на предыдущий элемент
 };
-struct stack *newStack(int capacity) // функция для инициализации stack
+struct Stack // структура данных для представления стека
 {
-    struct stack *pt = (struct stack *)malloc(sizeof(struct stack)); // выделение динамической памяти для stack
-    pt->maxsize = capacity;                                          // определение емкости
-    pt->top = -1;                                                    // определение индекса вершины
-    pt->items = (int *)malloc(sizeof(int) * capacity);               // выделение динамической памяти для массива элементов
+    struct Item *top; // указатель на вершину стека
+};
+struct Stack *newStack() // функция для инициализации стека
+{
+    struct Stack *pt = (struct Stack *)malloc(sizeof(struct Stack));
+    pt->top = NULL; // инициализация вершины как пустого списка
     return pt;
 }
-int size(struct stack *pt) // функция для возврата размера stack
+int isEmpty(struct Stack *pt) // функция для проверки, пуст ли стек
 {
-    return pt->top + 1; // возвращение текущего индекса вершины
+    return pt->top == NULL;
 }
-int isEmpty(struct stack *pt) // функция для проверки, пуст stack или нет
+void push(struct Stack *pt, int x) // функция для добавления элемента в стек
 {
-    return pt->top == -1; // возвращение равенства индекса вершины -1
+    struct Item *pti = (struct Item *)malloc(sizeof(struct Item)); // выделение памяти для элемента стека
+    pti->data = x; // присвоение значения элементу стека
+    pti->prev = pt->top; // присвоение значения указателю
+    pt->top = pti; // присвоение значения вершине
+    printf("Inserting %d\n", x);
 }
-int isFull(struct stack *pt) // функция для проверки, заполнен ли stack или нет
+int peek(struct Stack *pt) // функция для возврата верхнего элемента стека
 {
-    return pt->top == pt->maxsize - 1; // возвращение максимальной емкости stack
-}
-void push(struct stack *pt, int x) // функция для добавления элемента `x` в stack
-{
-    if (isFull(pt)) // проверка, не заполнен ли уже stack
+    if (!isEmpty(pt))
     {
-        printf("Overflow\nProgram Terminated\n"); // вывод информации о том, что stack заполнен
-        exit(EXIT_FAILURE);                       // завершение программы
-    }
-    printf("Inserting %d\n", x); // вывод информации о том, что элемент добавляется в stack
-    pt->items[++pt->top] = x;    // добавление элемента и увеличиние индекса вершины
-}
-int peek(struct stack *pt) // функция для возврата верхнего элемента stack
-{
-    if (!isEmpty(pt)) // проверка на пустой stack
-    {
-        return pt->items[pt->top]; // возврат верхнего элемента
+        return pt->top->data;
     }
     else
     {
-        exit(EXIT_FAILURE); // завершение программы
+        exit(EXIT_FAILURE);
     }
 }
-int pop(struct stack *pt) // функция для извлечения верхнего элемента из stack
+int pop(struct Stack *pt) // функция для извлечения верхнего элемента из стека
 {
-    if (isEmpty(pt)) // проверка на пустой stack
+    if (isEmpty(pt))
     {
-        printf("Underflow\nProgram Terminated\n"); // вывод информации о том, что stack пустой
-        exit(EXIT_FAILURE);                        // завершение программы
+        printf("Underflow\nProgram Terminated\n");
+        exit(EXIT_FAILURE);
     }
-    printf("Removing %d\n", peek(pt)); // вывод сообщения о извлечении элемента
-    return pt->items[pt->top--];       // уменьшение размер stack на 1
+    int data = pt->top->data; // извлечение элемента
+    pt->top = pt->top->prev; // изменение значения вершины
+    printf("Removing %d\n", data);
+    return data;
 }
 int main()
 {
-    struct stack *pt = newStack(5);              // создание stack емкостью 5
-    push(pt, 1);                                 // добавление 1 в stack
-    push(pt, 2);                                 // добавление 2 в stack
-    push(pt, 3);                                 // добавление 3 в stack
+    struct Stack *pt = newStack(); // создание стека
+    push(pt, 1);                   // добавление 1 в стек
+    push(pt, 2);                   // добавление 2 в стек
+    push(pt, 3);                   // добавление 3 в стек
     printf("The top element is %d\n", peek(pt)); // вывод верхнего элемента
-    pop(pt);                                     // удаление верхнего элемента из stack
-    pop(pt);                                     // удаление верхнего элемента из stack
-    pop(pt);                                     // удаление верхнего элемента из stack
-    if (isEmpty(pt))                             // проверка, пуст stack или нет
+    pop(pt); // удаление верхнего элемента из стека
+    pop(pt); // удаление верхнего элемента из стека
+    pop(pt); // удаление верхнего элемента из стека
+    if (isEmpty(pt)) // проверка, пуст ли стек
     {
-        printf("The stack is empty"); // вывод сообщения
+        printf("The stack is empty\n");
     }
     else
     {
-        printf("The stack is not empty"); // вывод сообщения
+        printf("The stack is not empty\n");
     }
     return 0;
 }
